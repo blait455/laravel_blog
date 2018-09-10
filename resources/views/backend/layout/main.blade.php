f62162a220098fbaf2b6e5f3e3f0b4fe461fda49
root <root@localhost.localdomain>
2018-09-10 17:53:56 +0530

    Enhanced the admin post form part

--- resources/views/backend/layout/main.blade.php
+++ resources/views/backend/layout/main.blade.php
@@ -41,11 +41,7 @@
     @yield('content')
     <!-- /.content-wrapper -->
     <footer class="main-footer">
-        <div class="pull-right hidden-xs">
-            <b>Version</b> 2.3.6
-        </div>
-        <strong>Copyright &copy; 2014-2016 Indranil</a>.</strong> All rights
-        reserved.
+
     </footer>
 
 </div>
