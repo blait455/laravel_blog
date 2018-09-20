9031d4cd9d2adc40ce8db493b1db7d44f0b2514d
Indranil Samanta <codearchitectdeveloper@gmail.com>
2018-09-20 16:18:39 +0530

    changed the admin path

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -29,7 +29,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/admin/home';
+    protected $redirectTo = 'blog/admin/home';
 
     /**
      * Create a new controller instance.
--- resources/views/backend/layout/_partials/navbar.blade.php
+++ resources/views/backend/layout/_partials/navbar.blade.php
@@ -4,7 +4,7 @@
         <!-- mini logo for sidebar mini 50x50 pixels -->
         <span class="logo-mini"><b>M</b>B </span>
         <!-- logo for regular state and mobile devices -->
-        <span class="logo-lg"><b>MY</b>BLOG</span>
+        <span class="logo-lg"><b>MY</b> ADMIN</span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
