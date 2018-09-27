be9b18f61da9eb8c80cf1b7bcfb69ab61ba08337
Indranil Samanta <codearchitectdeveloper@gmail.com>
2018-09-27 18:03:20 +0530

    Test javascript

--- resources/views/backend/blog/script.blade.php
+++ resources/views/backend/blog/script.blade.php
@@ -1,72 +1,73 @@
 
 @section('script')
     <script type="text/javascript">
-
-        $('#title').on('blur', function(){
-            var theTitle = this.value.toLowerCase().trim(),
-                slugInput = $('#slug'),
-                theSlug = theTitle.replace(/&/g, '-and-')
-                    .replace(/[^a-z0-9-]+/g, '-')
-                    .replace(/\-\-+/g, '-')
-                    .replace(/^-+|-+$/g, '');
-
-            slugInput.val(theSlug);
-        });
-
-        // var simplemde1 = new SimpleMDE({ element: $("#excerpt")[0] });
-        //var simplemde = new SimpleMDE({ element: $("#body")[0] });
-        //tinymce.init({ selector:'textarea' });
-
-        $('#datetimepicker1').datetimepicker({
-            format: 'YYYY-MM-DD HH:mm:ss',
-            showClear: true
-        });
-
-        $('#draft-btn').click(function (e) {
-            e.preventDefault();
-            $('#published_at').val("");
-            $('#post-form').submit();
-        });
-
         function clickHandler() {
             document.getElementById("draft").value = true;
         }
 
-    </script>
-    <script type="text/javascript">
-        var editor_config = {
-            path_absolute : "/",
-            selector: "#body.my-editor",
-            plugins: [
-                "advlist autolink lists link image charmap print preview hr anchor pagebreak",
-                "searchreplace wordcount visualblocks visualchars code fullscreen",
-                "insertdatetime media nonbreaking save table contextmenu directionality",
-                "emoticons template paste textcolor colorpicker textpattern"
-            ],
-            toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
-            relative_urls: false,
-            file_browser_callback : function(field_name, url, type, win) {
-                var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
-                var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
+        $(document).ready(function(){
 
-                var cmsURL = editor_config.path_absolute + 'laravel-filemanager?field_name=' + field_name;
-                if (type == 'image') {
-                    cmsURL = cmsURL + "&type=Images";
-                } else {
-                    cmsURL = cmsURL + "&type=Files";
+            $('#title').on('blur', function(){
+                var theTitle = this.value.toLowerCase().trim(),
+                    slugInput = $('#slug'),
+                    theSlug = theTitle.replace(/&/g, '-and-')
+                        .replace(/[^a-z0-9-]+/g, '-')
+                        .replace(/\-\-+/g, '-')
+                        .replace(/^-+|-+$/g, '');
+
+                slugInput.val(theSlug);
+            });
+
+            $('#draft-btn').click(function (e) {
+                e.preventDefault();
+                $('#published_at').val("");
+                $('#post-form').submit();
+            });
+
+            // function clickHandler() {
+            //     // document.getElementById("draft").value = true;
+            //     alert("Hello");
+            // }
+
+            $('#datetimepicker1').datetimepicker({
+                format: 'YYYY-MM-DD HH:mm:ss',
+                showClear: true
+            });
+
+            var editor_config = {
+                path_absolute : "/",
+                selector: "#body.my-editor",
+                plugins: [
+                    "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+                    "searchreplace wordcount visualblocks visualchars code fullscreen",
+                    "insertdatetime media nonbreaking save table contextmenu directionality",
+                    "emoticons template paste textcolor colorpicker textpattern"
+                ],
+                toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
+                relative_urls: false,
+                file_browser_callback : function(field_name, url, type, win) {
+                    var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
+                    var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
+
+                    var cmsURL = editor_config.path_absolute + 'laravel-filemanager?field_name=' + field_name;
+                    if (type == 'image') {
+                        cmsURL = cmsURL + "&type=Images";
+                    } else {
+                        cmsURL = cmsURL + "&type=Files";
+                    }
+
+                    tinyMCE.activeEditor.windowManager.open({
+                        file : cmsURL,
+                        title : 'Filemanager',
+                        width : x * 0.8,
+                        height : y * 0.8,
+                        resizable : "yes",
+                        close_previous : "no"
+                    });
                 }
+            };
+            tinymce.init(editor_config);
+        });
 
-                tinyMCE.activeEditor.windowManager.open({
-                    file : cmsURL,
-                    title : 'Filemanager',
-                    width : x * 0.8,
-                    height : y * 0.8,
-                    resizable : "yes",
-                    close_previous : "no"
-                });
-            }
-        };
-
-        tinymce.init(editor_config);
     </script>
 @endsection
